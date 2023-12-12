102f87f27ee435f1bfee74f9fed729e8f98b909d
Bulatovic Vuko <bulatovic.vuko@gmail.com>
2023-12-12 20:30:25 +0100

    checkpoint.

--- src/test/java/Pages/InventoryPage.java
+++ src/test/java/Pages/InventoryPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.net.IDN;
 import java.util.List;
 
 public class InventoryPage extends BaseTest {
